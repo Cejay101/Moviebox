498ddb5501e7a84d0a92222f76996fd138749306
Clinton Joy <Ceejay@clinton.local>
2023-09-12 19:01:19 +0100

    help me ooo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
-import Navbar from './components/Navbar';
 
 import { useLocation } from 'react-router-dom';
+import Header from './components/Header';
 function App() {
   const location = useLocation();
 
   return (
   <div>
-    <Navbar/>
+    <Header/>
     
   </div>
   );
